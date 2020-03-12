d9165f10fb8a59cc9fe9736f46075732b4ddeea3
x5se7en9x <tnansci@gmail.com>
2020-03-12 14:17:43 -0400

    2.1 commit
    added an object, with boxNo and count as properties

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -2,11 +2,12 @@
 
 // start count at 1
 let count = 1
-const gamePlay = function (event) {
 const countObject = {
-    count: [],
-    boxNo: []
-  }
+  count: [],
+  boxNo: []
+}
+
+const gamePlay = function (event) {
   const boxNo = event.target.id
   $(`#${boxNo}`).children('').removeClass('hidden')
   if (count % 2 === 1) {
@@ -14,15 +15,20 @@ const countObject = {
     // add 'X' into the box
     // $(`#${boxNo}`).children('p').text('X')
     $(`#${boxNo}`).children().attr('src', './../../public/x.png')
+    countObject.count.push(count)
+    countObject.boxNo.push(boxNo)
     count++
   } else if (count % 2 === 0) {
     console.log(`Turn: ${count}`)
     // add 'O'
     // $(`#${boxNo}`).children('p').text('O')
     $(`#${boxNo}`).children().attr('src', './../../public/o.png')
+    countObject.count.push(count)
+    countObject.boxNo.push(boxNo)
     count++
   }
   $(`#${boxNo}`).unbind()
+  console.log(countObject)
   return countObject
 }
 
