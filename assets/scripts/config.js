d5e6cdc8a51a0bdc94dccd12fcb831d1b9b3e7b9
x5se7en9x <tnansci@gmail.com>
2020-06-12 12:11:49 -0400

    Changed production environment from GA's to own

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://tic-tac-toe-wdi-production.herokuapp.com',
-  development: 'https://tic-tac-toe-wdi.herokuapp.com/'
+  production: 'https://mysterious-river-68675.herokuapp.com',
+  development: 'http://localhost:4741'
   // 'http://localhost:4741'
 }
 
